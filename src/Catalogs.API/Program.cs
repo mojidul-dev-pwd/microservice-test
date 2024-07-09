abd7e4553349912ca942eab3f0b36eb5381eac7f
Mojidul Islam <mojidul.dev.pwd@gmail.com>
2024-07-09 16:16:48 +0600

    add-catalog-controller

--- src/Catalogs.API/Program.cs
+++ src/Catalogs.API/Program.cs
@@ -1,4 +1,6 @@
 using Catalogs.API.HostingService;
+using Catalogs.API.Interfaces.Manager;
+using Catalogs.API.Manager;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHostedService<AppHostedService>();
+builder.Services.AddSingleton<IProductManager, ProductManager>();
 
 var app = builder.Build();
 
